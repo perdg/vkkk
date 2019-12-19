dae8dfc0fb97a8770a3f7019cc725630e07f6682
perdg <58484498+perdg@users.noreply.github.com>
2019-12-19 20:01:19 +0300

    supr
    supr

---
+++ precache-manifest.15c2d09c02a75a73bd3cae7d8dc66e9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7727099af54930a4b1949b2d7e71c25",
+    "url": "/vkkk/index.html"
+  },
+  {
+    "revision": "d0587c551b50b703d323",
+    "url": "/vkkk/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "1a0de7b98138ed8d6cdd",
+    "url": "/vkkk/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d0587c551b50b703d323",
+    "url": "/vkkk/static/js/2.5f764430.chunk.js"
+  },
+  {
+    "revision": "1a0de7b98138ed8d6cdd",
+    "url": "/vkkk/static/js/main.b047f3bc.chunk.js"
+  },
+  {
+    "revision": "5024350b9f205bf57439",
+    "url": "/vkkk/static/js/runtime-main.ff7ddaaa.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkkk/static/media/persik.4e1ec840.png"
+  }
+]);
